9a01337bc05ce9d673655e33104ad7d96c012da6
Johnson C <johnson.c@6dtech.co.in>
2018-08-10 17:07:52 +0530

    added login api to server

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,6 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
+app.get('/api/login', (req, res) => res.send({ isLoggedIn: true }));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(8081, () => console.log('Listening on port 8081!'));
