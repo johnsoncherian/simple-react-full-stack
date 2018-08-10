c147417c1daeb91d2d1c4fe1dfcdefd9098804bd
Johnson C <johnson.c@6dtech.co.in>
2018-08-10 18:15:50 +0530

    added logout api

--- src/server/index.js
+++ src/server/index.js
@@ -5,5 +5,6 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/login', (req, res) => res.send({ isLoggedIn: true }));
+app.get('/api/logout', (req, res) => res.send({ isLoggedIn: false }));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(8081, () => console.log('Listening on port 8081!'));
